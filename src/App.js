75361b340eb9167b1d57cf61077016b747009036
meetmoradiya <gaminghawkeye14@gmail.com>
2022-05-06 00:41:35 +0530

    added login page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import LoginForm from './Pages/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginForm />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
